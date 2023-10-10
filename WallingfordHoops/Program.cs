e165577cf4393c3ddfbc46dd451fe0c6d3c842f3
Henry Ho <henryho@microsoft.com>
2023-10-09 22:37:07 -0700

    Add method to get all games from spreadsheet. Add sample queries for getting player win rate and head to head win rate.

--- WallingfordHoops/Program.cs
+++ WallingfordHoops/Program.cs
@@ -21,7 +21,7 @@ using (var service = new SheetsService(new BaseClientService.Initializer()
 
     var getSpreadsheetRequest = service
         .Spreadsheets
-        .Get("1IpB80F49aSd8C5m1VvNJm2T12njdVSjzWl92k0cqpPM");
+        .Get(configuration["WallingfordHoopsSpreadsheetId"]);
     var spreadsheet = getSpreadsheetRequest.Execute();
 
     var rangesForSheetsWithGameData = new List<string>();
@@ -58,6 +58,40 @@ using (var service = new SheetsService(new BaseClientService.Initializer()
         Console.WriteLine();
     }
 
+    var games = GetAllGamesFromSpreadsheet(sheetsWithGameData);
+
+    var henryGamesWon = from game in games
+                        where game.Winners.Contains("Henry")
+                        select game;
+
+    var henryGamesLost = from game in games
+                         where game.Losers.Contains("Henry")
+                         select game;
+
+    var henryWinRate = 1.0 * henryGamesWon.Count() / (henryGamesWon.Count() + henryGamesLost.Count());
+
+    Console.WriteLine($"Henry's wins: {henryGamesWon.Count()}");
+    Console.WriteLine($"Henry's losses: {henryGamesLost.Count()}");
+    Console.WriteLine($"Henry's win rate: {Math.Round(henryWinRate, 3)}");
+
+    Console.WriteLine();
+
+    var henryVsGelGamesWon = from game in games
+                             where game.Winners.Contains("Henry")
+                             where game.Losers.Contains("Gel")
+                             select game;
+
+    var henryVsGelGamesLost = from game in games
+                              where game.Losers.Contains("Henry")
+                              where game.Winners.Contains("Gel")
+                              select game;
+
+    var henryVsGelWinRate = 1.0 * henryVsGelGamesWon.Count() / (henryVsGelGamesWon.Count() + henryVsGelGamesLost.Count());
+
+    Console.WriteLine($"Henry vs Gel wins: {henryVsGelGamesWon.Count()}");
+    Console.WriteLine($"Henry vs Gel losses: {henryVsGelGamesLost.Count()}");
+    Console.WriteLine($"Henry vs Gel win rate: {Math.Round(henryVsGelWinRate, 3)}");
+
     Console.WriteLine();
 
     // Stop timing
@@ -70,7 +104,7 @@ static void PrintRowDataAndGameRowData(GridData gridData, string gameDate)
     foreach (var rowData in gridData.RowData)
     {
         var rowValues = rowData.Values.Select(cellData => cellData.FormattedValue);
-        Console.WriteLine(String.Join(", ", rowValues));
+        Console.WriteLine(string.Join(", ", rowValues));
 
         var gameRowData = GameRowData.LoadFromRowData(rowData, gameDate);
         Console.WriteLine(gameRowData.Player);
@@ -90,4 +124,19 @@ static void PrintGames(string gameDate, GridData gridData)
         Console.WriteLine($"game.Losers: {string.Join(",", game.Losers)}");
         Console.WriteLine();
     }
+}
+
+static IList<Game> GetAllGamesFromSpreadsheet(IList<Sheet> spreadsheet)
+{
+    var games = new List<Game>();
+    foreach (var sheet in spreadsheet)
+    {
+        var gameDate = sheet.Properties.Title;
+        var gridData = sheet.Data[0];
+
+        var gameGridData = GameGridData.LoadFromGridData(gridData, gameDate);
+        games.AddRange(gameGridData.Games);
+    }
+
+    return games;
 }
